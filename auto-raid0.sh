11925473032ab9be2bf9be89de1252e1599804fa
elopezf-cenfo <elopezf_@outlook.com>
2019-02-24 22:47:49 -0600

    add orig raid creation script

---
+++ auto-raid0.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+parted --script /dev/sdc 'mklabel gpt mkpart primary 1MiB 4000GiB'
+parted --script /dev/sdd 'mklabel gpt mkpart primary 1MiB 4000GiB'
+parted --script /dev/sde 'mklabel gpt mkpart primary 1MiB 4000GiB'
+parted --script /dev/sdf 'mklabel gpt mkpart primary 1MiB 4000GiB'
+parted --script /dev/sdg 'mklabel gpt mkpart primary 1MiB 4000GiB'
+parted --script /dev/sdh 'mklabel gpt mkpart primary 1MiB 4000GiB'
+parted --script /dev/sdi 'mklabel gpt mkpart primary 1MiB 4000GiB'
+parted --script /dev/sdj 'mklabel gpt mkpart primary 1MiB 4000GiB'
+
+# gotta wait to catch them all. parted used udev to fire asynchronously,
+# so if we just move to mdadm, it will fail and then everthing goes
+# .
+
+while [ ! -e "/dev/sdc1" ]; do sleep 1; done
+while [ ! -e "/dev/sdd1" ]; do sleep 1; done
+while [ ! -e "/dev/sde1" ]; do sleep 1; done
+while [ ! -e "/dev/sdf1" ]; do sleep 1; done
+while [ ! -e "/dev/sdg1" ]; do sleep 1; done
+while [ ! -e "/dev/sdh1" ]; do sleep 1; done
+while [ ! -e "/dev/sdi1" ]; do sleep 1; done
+while [ ! -e "/dev/sdj1" ]; do sleep 1; done
+
+mdadm --create /dev/md127 --level 0 --raid-devices 8 /dev/sdc1 /dev/sdd1 /dev/sde1 /dev/sdf1 /dev/sdg1 /dev/sdh1 /dev/sdi1 /dev/sdj1
+
+mkfs -t ext4 /dev/md127
+
+mkdir /data
+
+uuid=$(blkid -o value -s UUID /dev/md127)
+echo "UUID=$uuid     /data   ext4    defaults,discard        0       2" >> /etc/fstab
+mount -a
+echo "bootdegraded=true" >> /etc/default/grub
